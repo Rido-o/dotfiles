dd8943edf902b72590c7498895c510e9dbd6804c
Rido <rido@airmail.cc>
2021-04-11 04:05:23 +0000

    added xmenu script for choosing dwm layout

---
+++ .local/bin/scripts/layoutmenu.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cat <<EOF | xmenu
+[]= Tiled Layout	0
+><> Floating Layout	1
+[M] Monocle Layout	2
+TTT Bstack Layout	3
+|M| Centered Master Layout	4
+(@) Spiral Layout	5
+HHH Grid Layout		6
+::: Gapless Grid Layout		7
+EOF
+
