e5940a2d72e9e177181add57947094edabf33f66
Rido <rido@airmail.cc>
2021-04-19 07:12:01 +0000

    added template for qutebrowser custome page css

---
+++ .config/qutebrowser/greasemonkey/Template.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @user Custom Css
+// @namespace namspace
+// @inject-into content
+// @match url scheme to match
+// @grant GM_addstyle
+// ==/UserScript==
+GM_addstyle(`
+	Custom css goes here
+`);
