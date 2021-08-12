725782c6708167f28248175c3adca096a33bb47b
Rido <rido@airmail.cc>
2021-08-12 21:11:37 +1000

    added a comment

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -15,6 +15,7 @@ map ('n', '<CR>', ':noh<CR>', opts)
 -- Replace object with default register without yanking replaced text
 map ('n', 'r', '\'"_c\' . (nr2char(getchar())) . (nr2char(getchar())) . "<C-r>+<esc>"', { expr = true })
 -- Two character replace motions
+-- * these are questionable
 map ('n', 'rr', '"_cc<C-r>+<BS><esc>', opts)
 map ('n', 'r0', '"_c0<C-r>+<BS><esc>', opts)
 map ('n', 'r$', '"_c$<C-r>+<BS><esc>', opts)
