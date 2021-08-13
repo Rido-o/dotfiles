669575fa74f65e0455b4ad054e4084bf2bfbe95f
Rido <rido@airmail.cc>
2021-08-13 17:47:41 +1000

    removed last of old vim files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,16 +20,6 @@ function SourceFile(file)
     vim.cmd("source " .. osPath .. file)
 end
 
---SourceFile('/plugged.vim')					-- Has file setting
---SourceFile('/settings.vim')				    -- Has ignorable file setting
-
---SourceFile('/colorschemes.vim')
---SourceFile('/plugin-settings.vim')
---SourceFile('/remaps-basics.vim')
---SourceFile('/which-key.vim')
---SourceFile('/which-key-local-leader.vim')
---SourceFile('/start-page.vim')                 -- Has file setting
-
 -- Main files
 require('plugin-loader')
 require('plugins')
