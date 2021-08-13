0f397cc60e8b077ebde4010412c1ef9cec7b9b9b
Rido <rido@airmail.cc>
2021-08-13 17:46:11 +1000

    fixed vimwiki settings

--- .config/nvim/lua/plugin-settings/vimwiki.lua
+++ .config/nvim/lua/plugin-settings/vimwiki.lua
@@ -2,10 +2,14 @@
 -- Settings
 -----------------
 -- Set vimwiki location
---if vim.g.os == "Windows" then
-    --vim.g.vimwiki_list = {['path'] = '~/Documents/Wiki/'}
---elseif vim.g.os == "Linux" then
-    --vim.g.vimwiki_list = {['path'] = '$HOME/.cfg/wiki/'}
---end
+if vim.g.os == "Windows" then
+    vim.g.vimwiki_list = {
+        {['path'] = '~/Documents/Wiki/'}
+    }
+elseif vim.g.os == "Linux" then
+    vim.g.vimwiki_list = {
+        {['path'] = '$HOME/.cfg/wiki/'}
+    }
+end
 -- Set vimwiki prefix
 vim.g.vimwiki_map_prefix = '<space>W'
