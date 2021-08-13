607eb70a289aad18c51dd18d88ef71256739e994
Rido <rido@airmail.cc>
2021-08-13 17:48:32 +1000

    Formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,19 @@
 -- Get OS
 vim.cmd([[
-if !exists("g:os")
-    if has("win64") || has("win32") || has("win16")
-        let g:os = "Windows"
+if !exists('g:os')
+    if has('win64') || has('win32') || has('win16')
+        let g:os = 'Windows'
     else
         let g:os = substitute(system('uname'), '\n', '', '')
     endif
 endif
 ]])
 
--- Source Files
-if vim.g.os == "Windows" then
-    osPath = "~/AppData/Local/nvim/config"
-elseif vim.g.os == "Linux" then
-    osPath = "$HOME/.config/nvim/config"
+-- Source files
+if vim.g.os == 'Windows' then
+    osPath = '~/AppData/Local/nvim/config'
+elseif vim.g.os == 'Linux' then
+    osPath = '$HOME/.config/nvim/config'
 end
 
 function SourceFile(file)
