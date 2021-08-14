9b2e704bfb22a9baedf2b512248a28ea1280a879
Rido <rido@airmail.cc>
2021-08-14 21:32:31 +1000

    removed airline themes from nvim colorschemes

--- .config/nvim/lua/colorschemes.lua
+++ .config/nvim/lua/colorschemes.lua
@@ -5,31 +5,23 @@ vim.o.background = 'dark'
 
 -- Gruvbox
 --vim.cmd('colorscheme gruvbox')
---vim.g.airline_theme = 'gruvbox'
 
 -- Onedark
 --vim.cmd('colorscheme onedark')
---vim.g.airline_theme = 'onedark'
 
 -- Pencil
 --vim.cmd('colorscheme pencil')
---vim.g.airline_theme = 'pencil'
 
 -- Tokyonight
 vim.g.tokyonight_style = 'night'
 vim.g.tokyonight_disable_italic_comment = '1'
 vim.g.tokyonight_lualine_bold = true
 vim.cmd('colorscheme tokyonight')
---vim.g.airline_theme = 'tokyonight'
 
 -- Miramare
 --vim.g.miramare_enable_italic = '1'
 --vim.g.miramare_disable_italic_comment = '1'
 --vim.cmd('colorscheme miramare')
---vim.g.airline_theme = 'miramare'
 
 -- Horizon
 --vim.cmd('colorscheme horizon')
-
--- Wal
---vim.cmd('colorscheme wal')
