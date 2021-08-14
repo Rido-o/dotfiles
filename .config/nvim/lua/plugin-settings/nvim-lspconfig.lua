70af9a932c852dbc6312fa2e57afa3eab6e5708c
Rido <rido@airmail.cc>
2021-08-15 00:10:36 +1000

    automatically set lsp config servers with lsp install

--- .config/nvim/lua/plugin-settings/nvim-lspconfig.lua
+++ .config/nvim/lua/plugin-settings/nvim-lspconfig.lua
@@ -34,10 +34,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = {
-    'python',
-    'lua'
-}
+local servers = require('lspinstall').installed_servers()
 
 for _, lsp in pairs(servers) do
     nvim_lsp[lsp].setup {
