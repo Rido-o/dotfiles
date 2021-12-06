e85de8ec14125a9aa15cec8a7e14157aeb98fbc2
Rido <rido@airmail.cc>
2021-12-06 17:37:04 +1100

    changed nvim lsp installer to setup servers instead of lsp config

--- .config/nvim/lua/plugin-settings/nvim-lspconfig.lua
+++ .config/nvim/lua/plugin-settings/nvim-lspconfig.lua
@@ -35,13 +35,13 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = require'nvim-lsp-installer.servers'.get_installed_server_names()
+-- local servers = require'nvim-lsp-installer.servers'.get_installed_server_names()
 
-for _, lsp in pairs(servers) do
-    nvim_lsp[lsp].setup{
-        on_attach = on_attach,
-        flags = {
-            debounce_text_changes = 150,
-        }
-    }
-end
+-- for _, lsp in ipairs(servers) do
+--     nvim_lsp[lsp].setup{
+--         on_attach = on_attach,
+--         flags = {
+--             debounce_text_changes = 150,
+--         }
+--     }
+-- end
