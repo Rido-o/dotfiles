7d98c8fe796afee443b685d05e6b9468c272e87a
Rido <rido@airmail.cc>
2021-12-07 12:52:19 +1100

    changed autopairs compe integration to cmp

--- .config/nvim/lua/plugin-settings/nvim-autopairs.lua
+++ .config/nvim/lua/plugin-settings/nvim-autopairs.lua
@@ -1,10 +1,16 @@
 require('nvim-autopairs').setup{}
 
-require("nvim-autopairs.completion.compe").setup{
-    map_cr = true, -- map <CR> on insert mode
-    map_complete = true, -- it will auto insert `(` after select function or method item
-    auto_select = false, -- auto select first item
-}
+-- nvim-cmp integration
+-- If you want to insert `(` after select function or method item
+-- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+-- local cmp = require('cmp')
+-- cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+
+-- require("nvim-autopairs.completion.compe").setup{
+--     map_cr = true, -- map <CR> on insert mode
+--     map_complete = true, -- it will auto insert `(` after select function or method item
+--     auto_select = false, -- auto select first item
+-- }
 
 ---- Treesitter Checking
 --local npairs = require("nvim-autopairs")
