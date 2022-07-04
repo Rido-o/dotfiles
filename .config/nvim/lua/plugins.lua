f638feab076ca35631486b1f0153d73ef15c55ba
Rido <rido@airmail.cc>
2022-07-04 23:18:29 +1000

    fixed treesitter?

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -167,7 +167,7 @@ return require('packer').startup({function(use)
     use {
        'nvim-treesitter/nvim-treesitter',
        run = ':TSUpdate',
-       -- config = plugin_config('plugin-settings.treesitter')
+       config = plugin_config('plugin-settings.treesitter')
     }
 
     ---------------
