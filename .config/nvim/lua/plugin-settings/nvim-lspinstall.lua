fd5c24d6f0229095d1e3cb01926a3bf422184089
Rido <rido@airmail.cc>
2022-07-04 22:51:27 +1000

    ensure pyright and sumneko_lua are installed with nvim-lsp-installer

--- .config/nvim/lua/plugin-settings/nvim-lspinstall.lua
+++ .config/nvim/lua/plugin-settings/nvim-lspinstall.lua
@@ -1,7 +1,7 @@
 require("nvim-lsp-installer").setup({
     -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer", "sumneko_lua" }
     -- This setting has no relation with the `automatic_installation` setting.
-    ensure_installed = {},
+    ensure_installed = {"pyright", "sumneko_lua"},
 
     -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
     -- This setting has no relation with the `ensure_installed` setting.
