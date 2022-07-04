83c6ba35774dea256a6cb3fd69dacf95b5e3e8cb
Rido <rido@airmail.cc>
2022-07-04 23:57:22 +1000

    modified lualine config and added lsp server name to it

--- .config/nvim/lua/plugin-settings/lualine.lua
+++ .config/nvim/lua/plugin-settings/lualine.lua
@@ -1,28 +1,38 @@
+local function lsp_name()
+    local msg = 'No Active Lsp'
+    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+        return msg
+    end
+    for _, client in ipairs(clients) do
+        local filetypes = client.config.filetypes
+        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+            return client.name
+        end
+    end
+    return msg
+end
+
 require('lualine').setup{
     options = {
-        -- Lualine colorschemes
-        -- theme = 'tokyonight',
-        theme = 'auto',
-
         icons_enabled = true,
+        theme = 'auto',
         component_separators = {'|', '|'}, -- {'', ''},
         section_separators = {'', ''}, -- {'', ''},
-        disabled_filetypes = {}
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        globalstatus = true
     },
     sections = {
         lualine_a = {'mode'},
-        lualine_b = {
-            'branch',
-            {
-                'diff',
-                color_added = nil,
-                color_modified = nil,
-                color_removed = nil,
-                symbols = {added = '+', modified = '~', removed = '-'}
-            }
-        },
-        lualine_d = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {
+            {lsp_name, icon = 'LSP:', separator = '|'},
+            'encoding',
+            'fileformat',
+            'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
     },
