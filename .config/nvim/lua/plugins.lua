c2ed4b5d752b752d78426f2569646bb2ed2c62b7
Rido <rido@airmail.cc>
2022-07-04 21:15:49 +1000

    comment change

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,7 +1,6 @@
 -- Packer Bootstrap
 local fn = vim.fn
--- Packer Install path
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim' -- Packer Install path
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
