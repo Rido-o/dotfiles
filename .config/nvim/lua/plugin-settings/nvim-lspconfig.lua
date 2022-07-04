56383c114b7b11e89d33f33e4df275c334030760
Rido <rido@airmail.cc>
2022-07-04 22:52:05 +1000

    formatting

--- .config/nvim/lua/plugin-settings/nvim-lspconfig.lua
+++ .config/nvim/lua/plugin-settings/nvim-lspconfig.lua
@@ -10,27 +10,27 @@ vim.keymap.set('n', '<space>aq', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>aw', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>aW', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>al', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>aD', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>ar', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>aa', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<space>af', vim.lsp.buf.formatting, bufopts)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>aw', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>aW', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>al', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>aD', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>ar', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>aa', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<space>af', vim.lsp.buf.formatting, bufopts)
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
