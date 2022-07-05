a3a1b8ef453ca4b0f5452b94ee537d61c1401ae3
Rido <rido@airmail.cc>
2022-07-05 23:02:04 +1000

    updated cmp bindings

--- .config/nvim/lua/plugin-settings/nvim-cmp.lua
+++ .config/nvim/lua/plugin-settings/nvim-cmp.lua
@@ -52,16 +52,12 @@ cmp.setup({
         -- completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     },
-    mapping = {
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-        }),
-        ['<CR>'] = cmp.mapping.confirm(),
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         -- Supertab bindings
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
@@ -83,7 +79,7 @@ cmp.setup({
                 fallback()
             end
         end, { "i", "s" }),
-    },
+    }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' }, -- For luasnip users.
