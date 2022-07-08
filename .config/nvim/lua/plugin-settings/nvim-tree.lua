57a79923f73375f97946f1788f00249a93cd46b2
Rido <rido@airmail.cc>
2022-07-08 21:38:18 +1000

    made changes to nvim-tree config

--- .config/nvim/lua/plugin-settings/nvim-tree.lua
+++ .config/nvim/lua/plugin-settings/nvim-tree.lua
@@ -1,72 +1,44 @@
 -----------------
 -- Settings
 -----------------
-require("nvim-tree").setup{
+require('nvim-tree').setup({
+    view = {
+        mappings = {
+            list = {
+                -- Key mappings
+                -- Don't won't l to go up a node if used on root folder
+                { key = { 'l', 'o', 'e', '<2-LeftMouse>' }, action = 'edit' },
+                -- Want h to go up a node if not closing a folder
+                { key = 'h', action = 'close_node' },
+                { key = '<BS>', action = 'dir_up'},
+                { key = 'v', action = 'vsplit' },
+                { key = 's', action = 'split' },
+                { key = 'n', action = 'create' },
+                -- Want <CR> to open a file if not used on a directory
+                { key = '<CR>', action = 'cd' }
+            },
+        },
+    },
     renderer = {
         indent_markers = {
             enable = true,
             icons = {
-                corner = "└ ",
-                edge = "│ ",
-                item = "│ ",
-                none = "  ",
+                corner = '└ ',
+                edge = '│ ',
+                item = '│ ',
+                none = '  ',
             },
         },
     },
-}
-
---------------------
--- Keybindings
---------------------
-
--- NvimTreeOpen and NvimTreeClose are also available if you need them
---nnoremap <C-n> :NvimTreeToggle<CR>
---nnoremap <leader>r :NvimTreeRefresh<CR>
---nnoremap <leader>n :NvimTreeFindFile<CR>
-
---set termguicolors -- this variable must be enabled for colors to be applied properly
-
--- a list of groups can be found at `:help nvim_tree_highlight`
---highlight NvimTreeFolderIcon guibg=blue
-
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-local treeMappings = {
-    { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
-    { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
-    { key = "<C-v>",                        cb = tree_cb("vsplit") },
-    { key = "<C-x>",                        cb = tree_cb("split") },
-    { key = "<C-t>",                        cb = tree_cb("tabnew") },
-    { key = "<",                            cb = tree_cb("prev_sibling") },
-    { key = ">",                            cb = tree_cb("next_sibling") },
-    { key = "P",                            cb = tree_cb("parent_node") },
-    { key = "<BS>",                         cb = tree_cb("close_node") },
-    { key = "<S-CR>",                       cb = tree_cb("close_node") },
-    { key = "<Tab>",                        cb = tree_cb("preview") },
-    { key = "K",                            cb = tree_cb("first_sibling") },
-    { key = "J",                            cb = tree_cb("last_sibling") },
-    { key = "I",                            cb = tree_cb("toggle_ignored") },
-    { key = "H",                            cb = tree_cb("toggle_dotfiles") },
-    { key = "R",                            cb = tree_cb("refresh") },
-    { key = "a",                            cb = tree_cb("create") },
-    { key = "d",                            cb = tree_cb("remove") },
-    { key = "r",                            cb = tree_cb("rename") },
-    { key = "<C-r>",                        cb = tree_cb("full_rename") },
-    { key = "x",                            cb = tree_cb("cut") },
-    { key = "c",                            cb = tree_cb("copy") },
-    { key = "p",                            cb = tree_cb("paste") },
-    { key = "y",                            cb = tree_cb("copy_name") },
-    { key = "Y",                            cb = tree_cb("copy_path") },
-    { key = "gy",                           cb = tree_cb("copy_absolute_path") },
-    { key = "[c",                           cb = tree_cb("prev_git_item") },
-    { key = "]c",                           cb = tree_cb("next_git_item") },
-    { key = "-",                            cb = tree_cb("dir_up") },
-    { key = "s",                            cb = tree_cb("system_open") },
-    { key = "q",                            cb = tree_cb("close") },
-    { key = "g?",                           cb = tree_cb("toggle_help") },
-}
+})
 
 -------------------
 -- Highlights
 -------------------
+-- a list of groups can be found at `:help nvim-tree-highlight`
+-- Main window bg and text color
 vim.cmd('hi! def link NvimTreeNormal Normal')
-vim.cmd('hi! def link NvimTreeVertSplit SignColumn')
+-- Window Picker color
+vim.cmd('hi! def link NvimTreeWindowPicker StatusLine')
+-- Seperator color
+vim.cmd('hi! def link NvimTreeWinSeparator SignColumn')
