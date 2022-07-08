d962e3609686e40fe2ffc97e95c1c7d903ca4584
Rido <rido@airmail.cc>
2022-07-08 22:14:38 +1000

    added custom function for open keybind in nvim-tree

--- .config/nvim/lua/plugin-settings/nvim-tree.lua
+++ .config/nvim/lua/plugin-settings/nvim-tree.lua
@@ -1,13 +1,29 @@
 -----------------
 -- Settings
 -----------------
+
+local lib = require('nvim-tree.lib')
+
+local function edit_light()
+    local action = 'edit'
+    local node = lib.get_node_at_cursor()
+
+    if node.link_to and not node.nodes then
+        require('nvim-tree.actions.open-file').fn(action, node.link_to)
+    elseif node.nodes ~= nil then
+        lib.expand_or_collapse(node)
+    elseif node.name ~= '..' then
+        require('nvim-tree.actions.open-file').fn(action, node.absolute_path)
+    end
+end
+
 require('nvim-tree').setup({
     view = {
         mappings = {
             list = {
                 -- Key mappings
                 -- Don't won't l to go up a node if used on root folder
-                { key = { 'l', 'o', 'e', '<2-LeftMouse>' }, action = 'edit' },
+                { key = { 'l', 'o', 'e', '<2-LeftMouse>' }, action = 'edit', action_cb = edit_light },
                 -- Want h to go up a node if not closing a folder
                 { key = 'h', action = 'close_node' },
                 { key = '<BS>', action = 'dir_up'},
