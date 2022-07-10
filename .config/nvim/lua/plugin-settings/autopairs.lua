681408a2fbba1955d52dccfb55bd18a3d2a8980b
Rido <rido@airmail.cc>
2022-07-10 12:26:20 +1000

    format autopairs.lua

--- .config/nvim/lua/plugin-settings/autopairs.lua
+++ .config/nvim/lua/plugin-settings/autopairs.lua
@@ -1,5 +1,5 @@
-require('nvim-autopairs').setup{
-    disable_filetype = { "TelescopePrompt" },
+require('nvim-autopairs').setup({
+    disable_filetype = { 'TelescopePrompt' },
     disable_in_macro = false, -- disable when recording or executing a macro
     disable_in_visualblock = false, -- disable when insert after visual block mode
     ignored_next_char = [=[[%w%%%'%[%"%.]]=],
@@ -13,13 +13,10 @@ require('nvim-autopairs').setup{
     map_bs = true, -- map the <BS> key
     map_c_h = false, -- Map the <C-h> key to delete a pair
     map_c_w = false, -- map <c-w> to delete a pair if possible
-}
+})
 
 -- nvim-cmp integration, add () for lsp functions, test with require in lua
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
-cmp.event:on(
-    'confirm_done',
-    cmp_autopairs.on_confirm_done()
-)
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
