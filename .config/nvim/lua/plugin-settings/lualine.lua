c6cc579901b3d40996334a6f2d322216cfe0bb40
Rido <rido@airmail.cc>
2022-07-11 20:18:01 +1000

    don't display null-ls in lualine

--- .config/nvim/lua/plugin-settings/lualine.lua
+++ .config/nvim/lua/plugin-settings/lualine.lua
@@ -7,7 +7,7 @@ local function lsp_name()
     end
     for _, client in ipairs(clients) do
         local filetypes = client.config.filetypes
-        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 and client.name ~= 'null-ls' then
             return client.name
         end
     end
