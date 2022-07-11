6b52631f9a8db058c2358e637721bbd5c17b7916
Rido <rido@airmail.cc>
2022-07-11 16:38:33 +1000

    more changed to bufferline config in terms of colors

--- .config/nvim/lua/plugin-settings/bufferline.lua
+++ .config/nvim/lua/plugin-settings/bufferline.lua
@@ -4,27 +4,61 @@ require('bufferline').setup({
         numbers = function(opts)
             return string.format('%s', opts.raise(opts.ordinal))
         end,
-        separator_style = {'▎', '▎'},
+        separator_style = { '▎', '▎' }, -- ▎,▐
         -- separator_style = 'thin',
+        indicator_icon = '▎',
         show_buffer_close_icons = false,
         show_buffer_default_icon = true,
         show_close_icon = false,
-        offsets = { { filetype = 'NvimTree', text = 'File Explorer', text_align = 'center' } },
+        offsets = {
+            { filetype = 'NvimTree', text = 'File Explorer', text_align = 'center' },
+            { filetype = 'undotree', text = 'Undo Tree', text_align = 'center' },
+        },
     },
     -- Visible might be in the window but not selected
     highlights = {
         -- Bar Color
-        fill = {
+        -- fill = {
+        --     guibg = {
+        --         attribute = 'bg',
+        --         highlight = 'TabLineFill',
+        --     },
+        -- },
+        -- Selected buffer colors
+        buffer_selected = {
+            gui = 'NONE',
+        },
+        -- Inactive visible buffer colors
+        buffer_visible = {
+            guifg = {
+                attribute = 'fg',
+                highlight = 'Comment',
+            },
             guibg = {
                 attribute = 'bg',
-                highlight = 'TabLineFill',
+                highlight = 'TabLine',
+            },
+        },
+        -- Inactive buffer colors
+        buffer = {
+            guifg = {
+                attribute = 'fg',
+                highlight = 'Comment',
+            },
+            guibg = {
+                attribute = 'bg',
+                highlight = 'TabLine',
             },
         },
         -- Tab Seperator color
-        separator_selected = {
+        separator = {
             guifg = {
-                attribute = 'fg',
-                highlight = 'Special',
+                attribute = 'bg',
+                highlight = 'Normal',
+            },
+            guibg = {
+                attribute = 'bg',
+                highlight = 'Tabline',
             },
         },
         -- Selected buffer indicator color
@@ -34,30 +68,48 @@ require('bufferline').setup({
                 highlight = 'Special',
             },
         },
-        -- Selected buffer
-        buffer_selected = {
-            gui = 'NONE',
-        },
-        -- Modified icon color for inactive buffers
-        modified = {
-            guifg = {
-                attribute = 'fg',
-                highlight = 'Special',
+        -- Visible buffer indicator color
+        indicator_visible = {
+            guibg = {
+                attribute = 'bg',
+                highlight = 'TabLine',
             },
         },
         -- Modified icon color for active buffers
         modified_selected = {
             guifg = {
                 attribute = 'fg',
-                highlight = 'Special',
+                highlight = 'WarningMsg',
             },
         },
-        -- Inactive buffer colors
-        background = {
+        -- Modified icon color for visible buffers
+        modified_visible = {
             guifg = {
                 attribute = 'fg',
-                highlight = 'Comment',
+                highlight = 'WarningMsg',
             },
+            guibg = {
+                attribute = 'bg',
+                highlight = 'TabLine',
+            },
+        },
+        -- Modified icon color for inactive buffers
+        modified = {
+            guifg = {
+                attribute = 'fg',
+                highlight = 'WarningMsg',
+            },
+            guibg = {
+                attribute = 'bg',
+                highlight = 'TabLine',
+            },
+        },
+        -- Color of selected buffer number
+        numbers_selected = {
+            gui = 'NONE',
+        },
+        -- Color of visible buffer number
+        numbers_visible = {
             guibg = {
                 attribute = 'bg',
                 highlight = 'TabLine',
