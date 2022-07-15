e5465b694eeb67e4ca6d557d35ce7a757ae79bbb
Rido <rido@airmail.cc>
2022-07-15 15:19:16 +1000

    formatting

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -54,9 +54,9 @@ require('nvim-surround').setup({
     },
 })
 
-vim.keymap.set("o", "ac", "a{", { buffer = true, desc = 'a block from { to } (curly brackets)' })
-vim.keymap.set("o", "ic", "i{", { buffer = true, desc = 'inner block from { to } (curly brackets)' })
-vim.keymap.set("o", "aa", "a<", { buffer = true, desc = 'a block from < to > (angled brackets)' })
-vim.keymap.set("o", "ia", "i<", { buffer = true, desc = 'inner block from < to > (angled brackets)' })
-vim.keymap.set("o", "ar", "a[", { buffer = true, desc = 'a block from [ to ] (rectangle brackets)' })
-vim.keymap.set("o", "ir", "i[", { buffer = true, desc = 'inner block from [ to ] (rectangle brackets)' })
+vim.keymap.set('o', 'ac', 'a{', { buffer = true, desc = 'a block from { to } (curly brackets)' })
+vim.keymap.set('o', 'ic', 'i{', { buffer = true, desc = 'inner block from { to } (curly brackets)' })
+vim.keymap.set('o', 'aa', 'a<', { buffer = true, desc = 'a block from < to > (angled brackets)' })
+vim.keymap.set('o', 'ia', 'i<', { buffer = true, desc = 'inner block from < to > (angled brackets)' })
+vim.keymap.set('o', 'ar', 'a[', { buffer = true, desc = 'a block from [ to ] (rectangle brackets)' })
+vim.keymap.set('o', 'ir', 'i[', { buffer = true, desc = 'inner block from [ to ] (rectangle brackets)' })
