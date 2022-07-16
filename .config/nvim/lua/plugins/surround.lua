db74e410f0d93e240a19766c5ddd5ee07938bcec
Rido <rido@airmail.cc>
2022-07-16 13:05:37 +1000

    removed default settings from surround config

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -3,8 +3,6 @@ require('nvim-surround').setup({
         insert = 's',
         insert_line = 'ss',
         visual = 's',
-        delete = 'ds',
-        change = 'cs',
     },
     delimiters = {
         pairs = {
@@ -16,41 +14,13 @@ require('nvim-surround').setup({
             ['>'] = { '< ', ' >' },
             ['['] = { '[', ']' },
             [']'] = { '[ ', ' ]' },
-            -- Define pairs based on function evaluations!
-            ['i'] = function()
-                return {
-                    require('nvim-surround.utils').get_input('Enter the left delimiter: '),
-                    require('nvim-surround.utils').get_input('Enter the right delimiter: '),
-                }
-            end,
-            ['f'] = function()
-                return {
-                    require('nvim-surround.utils').get_input('Enter the function name: ') .. '(',
-                    ')',
-                }
-            end,
-        },
-        separators = {
-            ["'"] = { "'", "'" },
-            ['"'] = { '"', '"' },
-            ['`'] = { '`', '`' },
-        },
-        HTML = {
-            ['t'] = 'type', -- Change just the tag type
-            ['T'] = 'whole', -- Change the whole tag contents
         },
         aliases = {
             ['a'] = '<', -- Single character aliases apply everywhere
             ['b'] = '(',
             ['c'] = '{',
             ['r'] = '[',
-            -- Table aliases only apply for changes/deletions
-            ['q'] = { '"', "'", '`' }, -- Any quote character
-            ['s'] = { ')', ']', '}', '>', "'", '"', '`' }, -- Any surrounding delimiter
         },
-    },
-    highlight_motion = { -- Highlight before inserting/changing surrounds
-        duration = 0,
     },
 })
 
