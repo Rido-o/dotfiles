ec5c8e5a174a7f6e4bbcbb407168de4233f08cd1
Rido <rido@airmail.cc>
2022-07-19 15:26:01 +1000

    formatting

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,10 +1,10 @@
 local function nmap(keys, func, opts)
-    opts = vim.tbl_extend('force', opts, { noremap = true, silent = true})
+    opts = vim.tbl_extend('force', opts, { noremap = true, silent = true })
     vim.keymap.set('n', keys, func, opts)
 end
 
 local function vmap(keys, func, opts)
-    opts = vim.tbl_extend('force', opts, { noremap = true, silent = true})
+    opts = vim.tbl_extend('force', opts, { noremap = true, silent = true })
     vim.keymap.set('v', keys, func, opts)
 end
 
@@ -12,13 +12,13 @@ end
 -- Basic
 --------------
 -- Enter to clear highlighting
-nmap('<CR>', ':noh<CR>', {desc = 'Clear highlighting'})
+nmap('<CR>', ':noh<CR>', { desc = 'Clear highlighting' })
 
 -- Do action without yanking into "+ register
-nmap('c', '"_c', {desc = 'Copy'})
-vmap('c', '"_c', {desc = 'Copy'})
-nmap('x', '"_x', {desc = 'Delete char'})
-vmap('x', '"_x', {desc = 'Delete char'})
+nmap('c', '"_c', { desc = 'Copy' })
+vmap('c', '"_c', { desc = 'Copy' })
+nmap('x', '"_x', { desc = 'Delete char' })
+vmap('x', '"_x', { desc = 'Delete char' })
 
 ----------------
 -- Replace
@@ -26,9 +26,9 @@ vmap('x', '"_x', {desc = 'Delete char'})
 -- Replace object with default register without yanking replaced text
 nmap('r', '\'"_c\' . (nr2char(getchar())) . (nr2char(getchar())) . "<C-r>+<esc>"', { expr = true })
 -- Two character replace motions
-nmap('rr', '"_cc<C-r>+<BS><esc>', {desc = 'Replace line'})
-nmap('r0', '"_c0<C-r>+<BS><esc>', {desc = 'Replace to start of line'})
-nmap('r$', '"_c$<C-r>+<BS><esc>', {desc = 'Replace to end of line'})
-nmap('r^', '"_c^<C-r>+<BS><esc>', {desc = 'Replace to start of line not including char'})
-nmap('rw', '"_cw<C-r>+<esc>', {desc = 'Replace word'})
-nmap('rW', '"_cW<C-r>+<esc>', {desc = 'Replace Word'})
+nmap('rr', '"_cc<C-r>+<BS><esc>', { desc = 'Replace line' })
+nmap('r0', '"_c0<C-r>+<BS><esc>', { desc = 'Replace to start of line' })
+nmap('r$', '"_c$<C-r>+<BS><esc>', { desc = 'Replace to end of line' })
+nmap('r^', '"_c^<C-r>+<BS><esc>', { desc = 'Replace to start of line not including char' })
+nmap('rw', '"_cw<C-r>+<esc>', { desc = 'Replace word' })
+nmap('rW', '"_cW<C-r>+<esc>', { desc = 'Replace Word' })
