9b04238aeacb92dcdbe9f510b08f8b5fce1680f2
Rido <rido@airmail.cc>
2022-07-24 14:02:04 +1000

    nvim autopairs removed default settings that were left unchanged and added treesitter settings

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -4,24 +4,16 @@ if not status_ok then
 end
 
 autopairs.setup({
-    disable_filetype = { 'TelescopePrompt' },
-    disable_in_macro = false, -- disable when recording or executing a macro
-    disable_in_visualblock = false, -- disable when insert after visual block mode
-    ignored_next_char = [=[[%w%%%'%[%"%.]]=],
-    enable_moveright = true,
-    enable_afterquote = true, -- add bracket pairs after quote
-    enable_check_bracket_line = true, -- check bracket in same line
-    enable_bracket_in_quote = true,
-    break_undo = true, -- switch for basic rule break undo sequence
-    check_ts = false,
-    map_cr = true,
-    map_bs = true, -- map the <BS> key
-    map_c_h = false, -- Map the <C-h> key to delete a pair
-    map_c_w = false, -- map <c-w> to delete a pair if possible
+    check_ts = true,
+    ts_config = {
+        lua = { 'string' }, -- it will not add a pair on that treesitter node
+    },
 })
 
 -- nvim-cmp integration, add () for lsp functions, test with require in lua
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+local cmp_status, cmp = pcall(require, 'cmp')
+if cmp_status then
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+end
