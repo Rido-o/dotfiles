56615cfb769d011344c5c6f3c133376c9bb0a322
Rido <rido@airmail.cc>
2022-07-25 18:34:57 +1000

    nvim ftplugin mappings streamlined

--- .config/nvim/ftplugin/lua.lua
+++ .config/nvim/ftplugin/lua.lua
@@ -1,1 +1,8 @@
-vim.keymap.set('n', '<localleader>a', function() print('hello')end, { desc = 'Print hello', buffer = 0 })
+local function map(lhs, rhs, opts)
+    opts = vim.tbl_extend('force', opts, { buffer = 0 })
+    vim.keymap.set('n', '<localleader>' .. lhs, rhs, opts)
+end
+
+map('a', function()
+    print('hello')
+end, { desc = 'Print hello' })
--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -1,3 +1,8 @@
-vim.keymap.set('n', '<localleader>r', '<CMD>1TermExec cmd=\'python "%"\' go_back=0<CR>', { desc = 'Run code', buffer = 0 })
-vim.keymap.set('n', '<localleader>b', '<CMD>1TermExec cmd=\'hyperfine --warmup 10 "python %"\' go_back=0<CR>', { desc = 'Benchmark code', buffer = 0 })
-vim.keymap.set('n', '<localleader>i', '<CMD>2TermExec cmd="python" go_back=0<CR>', { desc = 'Open repl', buffer = 0 })
+local function map(lhs, rhs, opts)
+    opts = vim.tbl_extend('force', opts, { buffer = 0 })
+    vim.keymap.set('n', '<localleader>' .. lhs, rhs, opts)
+end
+
+map('r', '<CMD>1TermExec cmd=\'python "%"\' go_back=0<CR>', { desc = 'Run code' })
+map('b', '<CMD>1TermExec cmd=\'hyperfine --warmup 10 "python %"\' go_back=0<CR>', { desc = 'Benchmark code' })
+map('i', '<CMD>2TermExec cmd="python" go_back=0<CR>', { desc = 'Open repl' })
