8fdab4bd7c060f08544772d455cd51f48ebbba4d
Rido <rido@airmail.cc>
2022-07-25 10:25:06 +1000

    nvim whichkey added replace 'r' so that it would show properly

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -3,7 +3,7 @@ if not status_ok then
     return
 end
 
-which_key.setup{
+which_key.setup({
     plugins = {
         marks = true, -- shows a list of your marks on ' and `
         registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
@@ -25,7 +25,7 @@ which_key.setup{
     },
     -- add operators that will trigger motion and text object completion
     -- to enable all native operators, set the preset / operators plugin above
-    operators = { gc = "Comments" },
+    operators = { gc = 'Comments', r = 'Replace' },
     key_labels = {
         -- override the label used to display some keys. It doesn't effect WK in any other way.
         -- For example:
@@ -34,13 +34,13 @@ which_key.setup{
         -- ["<tab>"] = "TAB",
     },
     icons = {
-        breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-        separator = "➜", -- symbol used between a key and it's label
-        group = "+", -- symbol prepended to a group
+        breadcrumb = '»', -- symbol used in the command line area that shows your active key combo
+        separator = '➜', -- symbol used between a key and it's label
+        group = '+', -- symbol prepended to a group
     },
     window = {
-        border = "none", -- none, single, double, shadow
-        position = "bottom", -- bottom, top
+        border = 'none', -- none, single, double, shadow
+        position = 'bottom', -- bottom, top
         margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
     },
@@ -48,21 +48,21 @@ which_key.setup{
         height = { min = 4, max = 25 }, -- min and max height of the columns
         width = { min = 20, max = 50 }, -- min and max width of the columns
         spacing = 3, -- spacing between columns
-        align = "left", -- align columns left, center or right
+        align = 'left', -- align columns left, center or right
     },
     ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-    hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
+    hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' }, -- hide mapping boilerplate
     show_help = true, -- show help message on the command line when the popup is visible
-    triggers = "auto", -- automatically setup triggers
+    triggers = 'auto', -- automatically setup triggers
     -- triggers = {"<leader>"} -- or specify a list manually
     triggers_blacklist = {
         -- list of mode / prefixes that should never be hooked by WhichKey
         -- this is mostly relevant for key maps that start with a native binding
         -- most people should not need to change this
-        i = { "j", "k" },
-        v = { "j", "k" },
+        i = { 'j', 'k' },
+        v = { 'j', 'k' },
     },
-}
+})
 
 -- Highlights
 vim.api.nvim_set_hl(0, 'WhichKey', { link = 'Function' })
@@ -89,82 +89,86 @@ end
 -- Normal Mode
 --------------------
 which_key.register({
+    r = { name = 'Replace' },
+})
+
+which_key.register({
     -- Single Mappings
-    s = {'<CMD>wincmd s<CR>' , 'Horizontal split'},
-    v = {'<CMD>wincmd v<CR>' , 'Vertical split'},
-    h = {'<CMD>wincmd h<CR>' , 'Move left'},
-    j = {'<CMD>wincmd j<CR>' , 'Move down'},
-    k = {'<CMD>wincmd k<CR>' , 'Move up'},
-    l = {'<CMD>wincmd l<CR>' , 'Move right'},
-    w = {'<CMD>w<CR>' , 'Write'},
-    q = {'<CMD>q<CR>' , 'Quit'},
-    U = {'<CMD>UndotreeToggle<CR>' , 'Open undo tree'},
-    c = {'<CMD>lua require("Comment.api").toggle_current_linewise()<CR>' , 'Toggle comment'},
+    s = { '<CMD>wincmd s<CR>', 'Horizontal split' },
+    v = { '<CMD>wincmd v<CR>', 'Vertical split' },
+    h = { '<CMD>wincmd h<CR>', 'Move left' },
+    j = { '<CMD>wincmd j<CR>', 'Move down' },
+    k = { '<CMD>wincmd k<CR>', 'Move up' },
+    l = { '<CMD>wincmd l<CR>', 'Move right' },
+    w = { '<CMD>w<CR>', 'Write' },
+    q = { '<CMD>q<CR>', 'Quit' },
+    U = { '<CMD>UndotreeToggle<CR>', 'Open undo tree' },
+    c = { '<CMD>lua require("Comment.api").toggle_current_linewise()<CR>', 'Toggle comment' },
 
     -- Buffers
     b = {
         name = 'Buffers',
-        q = {'<CMD>Bdelete<CR>' , 'Close buffer'},
-        Q = {'<CMD>w|%bd|e#|bd#<CR>', 'Close all except current'},
+        q = { '<CMD>Bdelete<CR>', 'Close buffer' },
+        Q = { '<CMD>w|%bd|e#|bd#<CR>', 'Close all except current' },
     },
 
-    f = { name = 'Find'},
+    f = { name = 'Find' },
 
     -- Lsp
     a = {
         name = 'Lsp',
-        a = {'<CMD>Lspsaga code_action<CR>' , 'Code action'},
-        r = {'<CMD>Lspsaga rename<CR>' , 'Rename'},
-        f = {'<CMD>lua vim.lsp.buf.formatting()<CR>' , 'Format'}
+        a = { '<CMD>Lspsaga code_action<CR>', 'Code action' },
+        r = { '<CMD>Lspsaga rename<CR>', 'Rename' },
+        f = { '<CMD>lua vim.lsp.buf.formatting()<CR>', 'Format' },
     },
 
     -- Fugitive
     g = {
         name = 'Git',
-        i = {'<CMD>Git init<CR>', 'Git init'},
-        a = {'<CMD>Git add .<CR>', 'Git add all'},
-        c = {'<CMD>Git commit<CR>', 'Git commit'},
-        A = {'<CMD>Git commit --amend --no-edit -a<CR>', 'Git amend last commit'},
-        r = {'<CMD>Git reset<CR>', 'Git unstage all'},
-        l = {'<CMD>Git log --decorate --pretty=format:"%h - %an, %ar : %s"<CR>', 'Git pretty log'},
-        L = {'<CMD>Git log<CR>', 'Git raw log'},
-        s = {'<CMD>Git status<CR>', 'Git status'},
-        b = {'<CMD>Git branch<CR>', 'Git branches'},
-        d = {'<CMD>Git diff<CR>', 'Git diff'},
-        I = {'<CMD>!touch .gitignore<CR>', 'Create git ignore'},
-        t = {'<CMD>SignifyToggle<CR>', 'Toggle signs'},
-        T = {'<CMD>SignifyToggleHighlight<CR>', 'Toggle highlights'},
+        i = { '<CMD>Git init<CR>', 'Git init' },
+        a = { '<CMD>Git add .<CR>', 'Git add all' },
+        c = { '<CMD>Git commit<CR>', 'Git commit' },
+        A = { '<CMD>Git commit --amend --no-edit -a<CR>', 'Git amend last commit' },
+        r = { '<CMD>Git reset<CR>', 'Git unstage all' },
+        l = { '<CMD>Git log --decorate --pretty=format:"%h - %an, %ar : %s"<CR>', 'Git pretty log' },
+        L = { '<CMD>Git log<CR>', 'Git raw log' },
+        s = { '<CMD>Git status<CR>', 'Git status' },
+        b = { '<CMD>Git branch<CR>', 'Git branches' },
+        d = { '<CMD>Git diff<CR>', 'Git diff' },
+        I = { '<CMD>!touch .gitignore<CR>', 'Create git ignore' },
+        t = { '<CMD>SignifyToggle<CR>', 'Toggle signs' },
+        T = { '<CMD>SignifyToggleHighlight<CR>', 'Toggle highlights' },
     },
 
     -- Vimwiki
     W = {
         name = 'Vimwiki',
-        i = {'<plug>VimwikiDiaryIndex' , 'Open Vimwiki diary'},
-        s = {'<plug>VimwikiUISelect' , 'Select Vimwiki'},
-        t = {'<plug>VimwikiTabIndex' , 'Tab Index'},
-        w = {'<plug>VimwikiIndex' , 'Open Vimwiki'},
+        i = { '<plug>VimwikiDiaryIndex', 'Open Vimwiki diary' },
+        s = { '<plug>VimwikiUISelect', 'Select Vimwiki' },
+        t = { '<plug>VimwikiTabIndex', 'Tab Index' },
+        w = { '<plug>VimwikiIndex', 'Open Vimwiki' },
         ['<space>'] = {
             name = 'Vimwiki Diary',
-            i = {'<plug>VimwikiDiaryGenerateLinks' , 'Generate links'},
-            m = {'<plug>VimwikiMakeTomorrowDiaryNote' , 'Tomorrows diary'},
-            t = {'<plug>VimwikiTabMakeDiaryNote' , 'Tab todays diary'},
-            w = {'<plug>VimwikiMakeDiaryNote' , 'Todays diary'},
-            y = {'<plug>VimwikiMakeYesterdayDiaryNote' , 'Yesterdays diary'},
+            i = { '<plug>VimwikiDiaryGenerateLinks', 'Generate links' },
+            m = { '<plug>VimwikiMakeTomorrowDiaryNote', 'Tomorrows diary' },
+            t = { '<plug>VimwikiTabMakeDiaryNote', 'Tab todays diary' },
+            w = { '<plug>VimwikiMakeDiaryNote', 'Todays diary' },
+            y = { '<plug>VimwikiMakeYesterdayDiaryNote', 'Yesterdays diary' },
         },
     },
 
     -- Toggle settings
     t = {
         name = 'Toggle',
-        c = {'<CMD>lua ColorColumn()<CR>' , 'Toggle color column'},
-    }
+        c = { '<CMD>lua ColorColumn()<CR>', 'Toggle color column' },
+    },
 }, { prefix = '<leader>' })
 
 --------------------
 -- Visual Mode
 --------------------
 which_key.register({
-    c = {'<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>' , 'Toggle comment'},
+    c = { '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', 'Toggle comment' },
 }, { mode = 'v', prefix = '<leader>' })
 
 -----------------------
@@ -172,41 +176,41 @@ which_key.register({
 -----------------------
 
 local setLocalKeybinds = function()
-    local fileTy = vim.api.nvim_buf_get_option(0, "filetype")
+    local fileTy = vim.api.nvim_buf_get_option(0, 'filetype')
     local opts = { prefix = '<localleader>', buffer = 0 }
 
     if fileTy == 'python' then
         which_key.register({
-            r = {'<CMD>1TermExec cmd=\'python "%"\' go_back=0<CR>', 'Run code'},
-            b = {'<CMD>1TermExec cmd=\'hyperfine --warmup 10 "python %"\' go_back=0<CR>', 'Benchmark code'},
-            i = {'<CMD>2TermExec cmd="python" go_back=0<CR>', 'Open repl'},
+            r = { '<CMD>1TermExec cmd=\'python "%"\' go_back=0<CR>', 'Run code' },
+            b = { '<CMD>1TermExec cmd=\'hyperfine --warmup 10 "python %"\' go_back=0<CR>', 'Benchmark code' },
+            i = { '<CMD>2TermExec cmd="python" go_back=0<CR>', 'Open repl' },
         }, opts)
     elseif fileTy == 'plaintex' or fileTy == 'tex' then
-		which_key.register({
-			i = {'<plug>(vimtex-info)' , 'Latex info'},
-			I = {'<plug>(vimtex-info-full)' , 'Latex full info'},
-			t = {'<plug>(vimtex-toc-open)' , 'Latex open toc'},
-			T = {'<plug>(vimtex-toc-toggle)' , 'Latex toggle toc'},
-			q = {'<plug>(vimtex-log)' , 'Latex log'},
-			v = {'<plug>(vimtex-view)' , 'Latex view'},
-			r = {'<plug>(vimtex-reverse-search)' , 'Latex reverse search'},
-			l = {'<plug>(vimtex-compile)' , 'Latex compile'},
-			L = {'<plug>(vimtex-compile-selected)' , 'Latex compile selected'},
-			k = {'<plug>(vimtex-stop)' , 'Latex stop'},
-			K = {'<plug>(vimtex-stop-all)' , 'Latex stop all'},
-			e = {'<plug>(vimtex-errors)' , 'Latex errors'},
-			o = {'<plug>(vimtex-compile-output)' , 'Latex compile output'},
-			g = {'<plug>(vimtex-status)' , 'Latex status'},
-			G = {'<plug>(vimtex-status-all)' , 'Latex status all'},
-			c = {'<plug>(vimtex-clean)' , 'Latex clean'},
-			C = {'<plug>(vimtex-clean-all)' , 'Latex clean all'},
-			m = {'<plug>(vimtex-imaps-list)' , 'Latex imaps list'},
-			x = {'<plug>(vimtex-reload)' , 'Latex reload'},
-			X = {'<plug>(vimtex-reload-state)' , 'Latex reload state'},
-			s = {'<plug>(vimtex-toggle-main)' , 'Latex toggle main'},
-		}, opts)
+        which_key.register({
+            i = { '<plug>(vimtex-info)', 'Latex info' },
+            I = { '<plug>(vimtex-info-full)', 'Latex full info' },
+            t = { '<plug>(vimtex-toc-open)', 'Latex open toc' },
+            T = { '<plug>(vimtex-toc-toggle)', 'Latex toggle toc' },
+            q = { '<plug>(vimtex-log)', 'Latex log' },
+            v = { '<plug>(vimtex-view)', 'Latex view' },
+            r = { '<plug>(vimtex-reverse-search)', 'Latex reverse search' },
+            l = { '<plug>(vimtex-compile)', 'Latex compile' },
+            L = { '<plug>(vimtex-compile-selected)', 'Latex compile selected' },
+            k = { '<plug>(vimtex-stop)', 'Latex stop' },
+            K = { '<plug>(vimtex-stop-all)', 'Latex stop all' },
+            e = { '<plug>(vimtex-errors)', 'Latex errors' },
+            o = { '<plug>(vimtex-compile-output)', 'Latex compile output' },
+            g = { '<plug>(vimtex-status)', 'Latex status' },
+            G = { '<plug>(vimtex-status-all)', 'Latex status all' },
+            c = { '<plug>(vimtex-clean)', 'Latex clean' },
+            C = { '<plug>(vimtex-clean-all)', 'Latex clean all' },
+            m = { '<plug>(vimtex-imaps-list)', 'Latex imaps list' },
+            x = { '<plug>(vimtex-reload)', 'Latex reload' },
+            X = { '<plug>(vimtex-reload-state)', 'Latex reload state' },
+            s = { '<plug>(vimtex-toggle-main)', 'Latex toggle main' },
+        }, opts)
     end
 end
 
 -- Autocmd to enable local keybinds -- Doesn't work with :file?
-vim.api.nvim_create_autocmd({'FileType'}, {callback = setLocalKeybinds})
+vim.api.nvim_create_autocmd({ 'FileType' }, { callback = setLocalKeybinds })
