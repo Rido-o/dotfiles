5aacdf4e519f1d0d7b6e88e81fa921bf64b2272f
Rido <rido@airmail.cc>
2022-07-25 20:42:36 +1000

    nvim colorschemes moved themes into a table to avoid those annoying warnings

--- .config/nvim/lua/core/colorschemes.lua
+++ .config/nvim/lua/core/colorschemes.lua
@@ -8,28 +8,25 @@ vim.o.background = 'dark'
 ----- Colorschemes -----
 ------------------------
 
--- Wal
-local function pywal()
-    setColorscheme('pywal')
-end
-
--- Tokyonight
-local function tokyonight()
-    vim.g.tokyonight_style = 'night'
-    vim.g.tokyonight_disable_italic_comment = '1'
-    setColorscheme('tokyonight')
-end
-
--- Kanagawa
-local function kanagawa()
-    require('kanagawa').setup({
-        globalStatus = true,
-    })
-    setColorscheme('kanagawa')
-end
+local themes = {
+    pywal = function()
+        setColorscheme('pywal')
+    end,
+    tokyonight = function()
+        vim.g.tokyonight_style = 'night'
+        vim.g.tokyonight_disable_italic_comment = '1'
+        setColorscheme('tokyonight')
+    end,
+    kanagawa = function()
+        require('kanagawa').setup({
+            globalStatus = true,
+        })
+        setColorscheme('kanagawa')
+    end,
+}
 
 ---------------------------
 ----- Selected Scheme -----
 ---------------------------
 
-pcall(kanagawa)
+pcall(themes.kanagawa)
