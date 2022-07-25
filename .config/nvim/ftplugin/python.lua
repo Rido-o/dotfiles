0113513f902fac7522c6fc0206cbcb815a3ce123
Rido <rido@airmail.cc>
2022-07-25 15:02:40 +1000

    nvim whichkey moved local bindings to ftplugin files

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -1,1 +1,3 @@
-print('hello')
+vim.keymap.set('r', '<CMD>1TermExec cmd=\'python "%"\' go_back=0<CR>', { desc = 'Run code' })
+vim.keymap.set('b', '<CMD>1TermExec cmd=\'hyperfine --warmup 10 "python %"\' go_back=0<CR>', { desc = 'Benchmark code' })
+vim.keymap.set('i', '<CMD>2TermExec cmd="python" go_back=0<CR>', { desc = 'Open repl' })
--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -6,3 +6,25 @@
 --autocmd FileType tex nnoremap <silent> <buffer> <F5> :w<CR>:AsyncRun -mode=term -pos=bottom -rows=10 latexmk -pdflatex='xelatex \%O \%S' -pdf "--interaction=batchmode" -f "%" <CR><C-\><C-N>
 --autocmd FileType tex nnoremap <silent> <buffer> <F5> :w<CR>:AsyncRun -mode=term -pos=bottom -rows=10 latexmk -pdflatex='xelatex \%O \%S' -pdf -f "%" <CR><C-\><C-N>
 --autocmd FileType tex nnoremap <silent> <buffer> <F5> :w<CR>:split<CR>:resize 10<CR>:ter clear;latexmk -pdflatex='xelatex \%O \%S' -pdf --interaction=batchmode -f "%" <CR><C-\><C-N>
+
+vim.keymap.set('<localleader>i', '<plug>(vimtex-info)', {desc = 'Latex info'} )
+vim.keymap.set('<localleader>I', '<plug>(vimtex-info-full)', {desc = 'Latex full info'} )
+vim.keymap.set('<localleader>t', '<plug>(vimtex-toc-open)', {desc = 'Latex open toc'} )
+vim.keymap.set('<localleader>T', '<plug>(vimtex-toc-toggle)', {desc = 'Latex toggle toc'} )
+vim.keymap.set('<localleader>q', '<plug>(vimtex-log)', {desc = 'Latex log'} )
+vim.keymap.set('<localleader>v', '<plug>(vimtex-view)', {desc = 'Latex view'} )
+vim.keymap.set('<localleader>r', '<plug>(vimtex-reverse-search)', {desc = 'Latex reverse search'} )
+vim.keymap.set('<localleader>l', '<plug>(vimtex-compile)', {desc = 'Latex compile'} )
+vim.keymap.set('<localleader>L', '<plug>(vimtex-compile-selected)', {desc = 'Latex compile selected'} )
+vim.keymap.set('<localleader>k', '<plug>(vimtex-stop)', {desc = 'Latex stop'} )
+vim.keymap.set('<localleader>K', '<plug>(vimtex-stop-all)', {desc = 'Latex stop all'} )
+vim.keymap.set('<localleader>e', '<plug>(vimtex-errors)', {desc = 'Latex errors'} )
+vim.keymap.set('<localleader>o', '<plug>(vimtex-compile-output)', {desc = 'Latex compile output'} )
+vim.keymap.set('<localleader>g', '<plug>(vimtex-status)', {desc = 'Latex status'} )
+vim.keymap.set('<localleader>G', '<plug>(vimtex-status-all)', {desc = 'Latex status all'} )
+vim.keymap.set('<localleader>c', '<plug>(vimtex-clean)', {desc = 'Latex clean'} )
+vim.keymap.set('<localleader>C', '<plug>(vimtex-clean-all)', {desc = 'Latex clean all'} )
+vim.keymap.set('<localleader>m', '<plug>(vimtex-imaps-list)', {desc = 'Latex imaps list'} )
+vim.keymap.set('<localleader>x', '<plug>(vimtex-reload)', {desc = 'Latex reload'} )
+vim.keymap.set('<localleader>X', '<plug>(vimtex-reload-state)', {desc = 'Latex reload state'} )
+vim.keymap.set('<localleader>s', '<plug>(vimtex-toggle-main)', {desc = 'Latex toggle main'} )
