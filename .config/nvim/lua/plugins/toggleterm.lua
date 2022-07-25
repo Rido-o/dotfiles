e092f90eaef4d1297d7d550523396f27c793e111
Rido <rido@airmail.cc>
2022-07-25 21:13:13 +1000

    nvim toggleterm messed with keybinds a little bit

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,15 +1,15 @@
 ----------------
 -- Options
 ----------------
-require("toggleterm").setup{
+require('toggleterm').setup({
     -- size can be a number or function which is passed the current terminal
     size = 15,
     --size = function(term)
-        --if term.direction == "horizontal" then
-            --return 15
-        --elseif term.direction == "vertical" then
-            --return vim.o.columns * 0.4
-        --end
+    --if term.direction == "horizontal" then
+    --return 15
+    --elseif term.direction == "vertical" then
+    --return vim.o.columns * 0.4
+    --end
     --end,
     open_mapping = [[<c-\>]],
     hide_numbers = true, -- hide the number column in toggleterm buffers
@@ -33,25 +33,23 @@ require("toggleterm").setup{
         height = '30',
         winblend = 3,
         highlights = {
-            border = "Normal",
-            background = "Normal",
-        }
-    }
-}
+            border = 'Normal',
+            background = 'Normal',
+        },
+    },
+})
 
 --------------------------
 -- Terminal Mappings
 --------------------------
 function _G.set_terminal_keymaps()
-    local map = vim.api.nvim_set_keymap
-    local opts = { noremap = true }
+    local map = vim.keymap.set
+    local opts = { noremap = true, buffer = 0 }
     map('t', '<esc>', [[<C-\><C-n>]], opts)
-    --map('t', 'jk', [[<C-\><C-n>]], opts)
-    --map('t', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-    --map('t', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-    --map('t', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-    --map('t', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+vim.api.nvim_create_autocmd(
+    { 'TermOpen' },
+    { pattern = 'term://*toggleterm#*', command = 'lua set_terminal_keymaps()' }
+)
