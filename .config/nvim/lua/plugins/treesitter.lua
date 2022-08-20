e71f9bc3b4c47d78cde38fc45e4f38bfea5332a0
Rido <rido@airmail.cc>
2022-08-20 14:34:10 +1000

    nvim added nix to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 treesitter_configs.setup({
-    ensure_installed = { 'lua', 'python' }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = { 'lua', 'python', 'nix' }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     --ignore_install = { "javascript" }, -- List of parsers to ignore installing
     highlight = {
         enable = true, -- false will disable the whole extension
