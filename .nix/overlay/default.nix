cdf8cc1cde53335a36880c78f8e5727b85d30795
Rido <rido@airmail.cc>
2022-08-21 13:44:26 +1000

    nix added xmenu overlay

--- .nix/overlay/default.nix
+++ .nix/overlay/default.nix
@@ -54,6 +54,23 @@ let
       };
       preBuild = "cp ${configFile} config.def.h";
     });
+    xmenu = super.xmenu.overrideAttrs (oldAttrs: rec {
+      src = super.fetchFromGitHub {
+        owner = "phillbush";
+        repo = "xmenu";
+        rev = "523b3d5e25b90f6336eb4085f611f635350ac9dc";
+        sha256 = "RVvU++diBz/LIKsu0j4+xZOG+QpoLypshvrUaSC1cNc=";
+      };
+      configFile = super.fetchurl {
+        url = "https://raw.githubusercontent.com/Rido-o/xmenu/master/config.h";
+        sha256 = "x2KBNUw051JBunrpW1tvaAuuX+KGPK1O5VMJgYYRLdE=";
+      };
+      postPatch = "
+        sed -i \"s:/usr/local:$out:\" Makefile
+        cp ${configFile} config.h
+      ";
+      # postPatch = "cp ${configFile} config.h";
+    });
   };
 in
 inputs.nixpkgs.lib.composeManyExtensions [ modifications ]
