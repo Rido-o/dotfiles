097b2565333d74731a76caf82a053b6e9ce7d9b0
Rido <rido@airmail.cc>
2022-08-21 13:15:53 +1000

    nix added nsxiv overlay

--- .nix/configuration.nix
+++ .nix/configuration.nix
@@ -99,6 +99,7 @@
   # List packages installed in system profile. To search, run: $ nix search wget
   environment.systemPackages = with pkgs; [
     st
+    nsxiv
     zsh
     zsh-syntax-highlighting
     zsh-autosuggestions
--- .nix/overlay/default.nix
+++ .nix/overlay/default.nix
@@ -39,6 +39,21 @@ let
       };
       postPatch = oldAttrs.postPatch + "cp ${configFile} config.def.h";
     });
+    nsxiv = super.nsxiv.overrideAttrs (oldAttrs: rec {
+      src = super.fetchFromGitea {
+        domain = "codeberg.org";
+        owner = "nsxiv";
+        repo = "nsxiv";
+        rev = "0f0c49a630285af10c765e0b724896ff281e7b66";
+        sha256 = "u56gJk7IzwJouawCoU+WGC87KMrhU1CFYvkPUTVRcGM=";
+      };
+      patches = [];
+      configFile = super.fetchurl {
+        url = "https://raw.githubusercontent.com/Rido-o/nsxiv/master/config.def.h";
+        sha256 = "Zo7EqtQ+JywJZQdmzGN79h6MEh7JPcuRQkuKxH1+x/k=";
+      };
+      preBuild = "cp ${configFile} config.def.h";
+    });
   };
 in
 inputs.nixpkgs.lib.composeManyExtensions [ modifications ]
