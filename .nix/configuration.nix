7c379c08ee8d80e7461d7685210f977731d22754
Rido <rido@airmail.cc>
2022-08-22 17:37:36 +1000

    nix added killall and some statusbar script dependencies

--- .nix/configuration.nix
+++ .nix/configuration.nix
@@ -98,6 +98,8 @@
 
   # List packages installed in system profile. To search, run: $ nix search wget
   environment.systemPackages = with pkgs; [
+    killall
+    dwmblocks sd fd ripgrep pamixer mpd ncmpcpp mpc-cli pulsemixer
     st
     nsxiv
     xmenu
