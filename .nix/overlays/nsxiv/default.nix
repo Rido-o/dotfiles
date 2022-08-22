789f21e65e6144c738393755f636ad98febfb21e
Rido <rido@airmail.cc>
2022-08-22 15:59:10 +1000

    nix added patch to nsxiv

--- .nix/overlays/nsxiv/default.nix
+++ .nix/overlays/nsxiv/default.nix
@@ -8,7 +8,12 @@ self: super: {
       rev = "0f0c49a630285af10c765e0b724896ff281e7b66";
       sha256 = "u56gJk7IzwJouawCoU+WGC87KMrhU1CFYvkPUTVRcGM=";
     };
-    patches = [ ];
+    patches = [
+      (super.fetchpatch {
+        url = "https://codeberg.org/nsxiv/nsxiv-extra/raw/branch/master/patches/load-dir/load-dir-3a22e6a.patch";
+        sha256 = "MrCUJlGt660CdMF/saeZzpshPiOKvbefWB7kCUdgDq4=";
+      })
+    ];
     configFile = super.fetchurl {
       url = "https://raw.githubusercontent.com/Rido-o/nsxiv/master/config.def.h";
       sha256 = "Zo7EqtQ+JywJZQdmzGN79h6MEh7JPcuRQkuKxH1+x/k=";
