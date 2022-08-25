ef09bb446e783ad859adabbd945d4722da490e57
Rido <rido@airmail.cc>
2022-08-25 21:06:02 +1000

    nix renamed home.nix and configuration.nix

--- .nix/flake.nix
+++ .nix/flake.nix
@@ -24,98 +24,100 @@
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, devshell, overlays, ... }:
-  let
-    host = "nixos";
-    user = "reid";
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-      overlays = with inputs; [
-        devshell.overlay
-        neovim-nightly-overlay.overlay
-      ] ++ overlays.overlays;
-    };
-  in {
-    # NixOS configurations
-    nixosConfigurations = {
-      ${host} = nixpkgs.lib.nixosSystem {
-        inherit system pkgs;
-        specialArgs = { inherit inputs host user; };
-        modules = [
-          ./nixos/configuration.nix
-          # Default home-manager user configuration
-          home-manager.nixosModules.home-manager {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = { inherit inputs user; };
-              users.${user} = import ./home-manager/home.nix;
-            };
+    let
+      host = "nixos";
+      user = "reid";
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = with inputs; [
+          devshell.overlay
+          neovim-nightly-overlay.overlay
+        ] ++ overlays.overlays;
+      };
+    in
+    {
+      # NixOS configurations
+      nixosConfigurations = {
+        ${host} = nixpkgs.lib.nixosSystem {
+          inherit system pkgs;
+          specialArgs = { inherit inputs host user; };
+          modules = [
+            ./nixos
+            # Default home-manager user configuration
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = { inherit inputs user; };
+                users.${user} = import ./home-manager;
+              };
+            }
+          ];
+        };
+      };
+
+      # Standalone home-manager configurations
+      homeConfigurations = {
+        "${user}@${host}" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = { inherit inputs user; };
+          modules = [ ./home-manager/home.nix ];
+        };
+      };
+    } //
+    # Commands
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShells.default = pkgs.devshell.mkShell {
+        devshell.motd = ''
+          {bold}{14}~ NixOS command center ~{reset}
+          $(type -p menu &>/dev/null && menu)
+        '';
+
+        commands = [
+          {
+            name = "dev:switch";
+            category = "Install";
+            help = "Updates changes made to configuration.nix";
+            command = ''
+              sudo nixos-rebuild switch --flake .#nixos
+            '';
+          }
+          {
+            name = "dev:update";
+            category = "Install";
+            help = "Updates the entire system and all packages";
+            command = ''
+              sudo nixos-rebuild switch --flake .#nixos --recreate-lock-file
+            '';
+          }
+          {
+            name = "dev:lock";
+            category = "Install";
+            help = "Updates lock file only";
+            command = ''
+              nix flake update
+            '';
+          }
+          {
+            name = "dev:input-switch";
+            category = "Install";
+            help = "Updates input and switch";
+            command = ''
+              sudo nixos-rebuild switch --flake .#nixos --update-input $1
+            '';
+          }
+          {
+            name = "dev:clean";
+            category = "Maintenance";
+            help = "Perform garbage collection and delete all old generations";
+            command = ''
+              sudo nix-collect-garbage
+            '';
           }
         ];
       };
-    };
-
-    # Standalone home-manager configurations
-    homeConfigurations = {
-      "${user}@${host}" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = { inherit inputs user; };
-        modules = [ ./home-manager/home.nix ];
-      };
-    };
-  } //
-  # Commands
-  flake-utils.lib.eachDefaultSystem (system: {
-    devShells.default = pkgs.devshell.mkShell {
-      devshell.motd = ''
-        {bold}{14}~ NixOS command center ~{reset}
-        $(type -p menu &>/dev/null && menu)
-      '';
-
-      commands = [
-        {
-          name = "dev:switch";
-          category = "Install";
-          help = "Updates changes made to configuration.nix";
-          command = "
-            sudo nixos-rebuild switch --flake .#nixos
-          ";
-        }
-        {
-          name = "dev:update";
-          category = "Install";
-          help = "Updates the entire system and all packages";
-          command = ''
-            sudo nixos-rebuild switch --flake .#nixos --recreate-lock-file
-          '';
-        }
-        {
-          name = "dev:lock";
-          category = "Install";
-          help = "Updates lock file only";
-          command = ''
-            nix flake update
-          '';
-        }
-        {
-          name = "dev:input-switch";
-          category = "Install";
-          help = "Updates input and switch";
-          command = ''
-            sudo nixos-rebuild switch --flake .#nixos --update-input $1
-          '';
-        }
-        {
-          name = "dev:clean";
-          category = "Maintenance";
-          help = "Perform garbage collection and delete all old generations";
-          command = ''
-            sudo nix-collect-garbage
-          '';
-        }
-      ];
-    };
-  });
+    });
 }
