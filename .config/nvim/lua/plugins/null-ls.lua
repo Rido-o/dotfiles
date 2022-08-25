65c95ee92fcf4b548000525b933184c97db28184
Rido <rido@airmail.cc>
2022-08-25 14:00:43 +1000

    nvim added format on save to null-ls

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -3,6 +3,7 @@ if not status_ok then
     return
 end
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
     sources = {
         require('null-ls').builtins.formatting.stylua,
@@ -11,4 +12,17 @@ null_ls.setup({
         require('null-ls').builtins.code_actions.statix,
         require('null-ls').builtins.diagnostics.statix,
     },
+    -- you can reuse a shared lspconfig on_attach callback here
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
+        end
+    end,
 })
