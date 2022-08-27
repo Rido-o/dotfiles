c135a73328a787a1ead609c715d185fd26156614
Rido <rido@airmail.cc>
2022-08-28 08:42:25 +1000

    nvim plugins formatting

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -3,7 +3,7 @@ local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim' -- Packer Install path
 if fn.empty(fn.glob(install_path)) > 0 then
     Packer_bootstrap =
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
 end
 
 local status_ok, packer = pcall(require, 'packer')
